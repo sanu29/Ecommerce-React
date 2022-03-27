4312564b38e288524367ea51e611b126d493dcfa
Saniya Anush Shaikh <sanu29121999@gmail.com>
2022-03-27 23:25:16 +0530

    updated : Temp Data updated for redering

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,56 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "dairy",
     description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+      "This category contains all the dairy products",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "staples",
     description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+      "This category contains all the pulses and dals",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "vegetables",
     description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+      "This category contains all vegetables",
+  },
+  {
+    _id: uuid(),
+    categoryName: "fruits",
+    description:
+      "This category contains all Fruits",
+  },
+  {
+    _id: uuid(),
+    categoryName: "snacks",
+    description:
+      "This category contains all snacks",
+  },
+  {
+    _id: uuid(),
+    categoryName: "sprouts",
+    description:
+      "This category contains all meatandchicken",
+  },
+  {
+    _id: uuid(),
+    categoryName: "dryfruits",
+    description:
+      "This category contains all dryfruits",
+  },
+  {
+    _id: uuid(),
+    categoryName: "spices",
+    description:
+      "This category contains all spices",
+  },
+  {
+    _id: uuid(),
+    categoryName: "sprouts",
+    description:
+      "This category contains all sprouts",
   },
 ];
