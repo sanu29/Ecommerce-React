fe5bf6a1f6e8deda4aaed9c6723640ea4f97c6a3
Saniya Anush Shaikh <sanu29121999@gmail.com>
2022-04-03 22:27:05 +0530

    fix: classnames refactored

--- src/component/ProductBody.jsx
+++ src/component/ProductBody.jsx
@@ -17,26 +17,26 @@ export const Products = () =>{
         <>         
             <div className="main">
              <div className="comodities d-flex justify-content-around flex-wrap">
-                 {products.map((prod)=>{
-                    return (<>
-                    <div class="card border-radius-sm margin-8 d-flex justify-center align-items-center">
-                    <div class="position-relative">
-                        <img class="card-img-main border-radius-sm margin-16 "
+                 {products.map((prod,key)=>{
+                    return (<li className="product-list"  key={prod.id} >
+                    <div className="card card-product border-radius-sm margin-8 d-flex justify-center align-items-center">
+                    <div className="position-relative">
+                        <img className="card-img-main card-img-main-product border-radius-sm margin-16 "
                             src={prod.image} alt="food"/>
                         <i
-                            class="material-icons btn btn-icon  position-absolute card-icon-dismiss btn-danger-outline">favorite</i>
+                            className="material-icons btn btn-icon  position-absolute card-icon-dismiss btn-danger-outline">favorite</i>
 
                     </div>
-                    <div class="card-primary">
-                        <div class="card-title text-color-primary font-weight-bold">{prod.title}</div>
-                        <h2 class="card-subtitle"><span class="text-muted">₹</span>{prod.price}/{prod.quantity}</h2>
+                    <div className="card-primary">
+                        <div className="card-title text-color-primary font-weight-bold">{prod.title}</div>
+                        <h2 className="card-subtitle"><span className="text-muted">₹</span>{prod.price}/{prod.quantity}</h2>
                     </div>
-                    <div class="card-action">
-                        <div class="btn border-radius-sm btn-primary w-100 margin-none text-align-center addtocart"> Add to cart
+                    <div className="card-action">
+                        <div  key={key}  className="btn border-radius-sm btn-primary w-100 margin-none text-align-center addtocart"> Add to cart
                         </div>
                     </div>
                 </div>
-                    </>)
+                    </li>)
                  
                  })}
                  
